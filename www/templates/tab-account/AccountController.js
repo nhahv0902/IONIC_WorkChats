7e8e4ea076894843b98e551ad4b4ce39f0fd2641
dodacvan <dodacvan1995@gmail.com>
2016-11-17 05:34:16 +0700

    addimage

--- www/templates/tab-account/AccountController.js
+++ www/templates/tab-account/AccountController.js
@@ -1,8 +1,9 @@
 /**
  * Created by Nhahv on 11/12/2016.
  */
-appControllers.controller('AccountCtrl', function ($scope, $ionicModal) {
+appControllers.controller('AccountCtrl', function ($scope, $ionicModal, $timeout) {
   $scope.change = {};
+  var storageRef = firebase.storage().ref();
   //noinspection JSUnresolvedVariable,JSUnresolvedFunction
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
@@ -16,6 +17,12 @@ appControllers.controller('AccountCtrl', function ($scope, $ionicModal) {
         $scope.change.useraddress = snapshot.val().address;
         $scope.change.username = snapshot.val().name;
         $scope.change.avatar = snapshot.val().avatar;
+        $scope.change.image = snapshot.val().image;
+        storageRef.child($scope.change.userId).child($scope.change.image).getDownloadURL().then(function(url) {
+          document.querySelector('#avatar').src = url;
+        }).catch(function(error) {
+          // Handle any errors
+        });
       });
     } else {
       console.log('not log in');
@@ -24,6 +31,28 @@ appControllers.controller('AccountCtrl', function ($scope, $ionicModal) {
   $scope.resetpass = function () {
     $scope.change.userpassword = '';
   };
+  $scope.thumbnail = [];
+    // Read the image using the filereader 
+  $scope.fileReaderSupported = window.FileReader != null;
+  $scope.photoChanged = function(files) {
+    if (files != null) {
+      var file = files[0];
+      $scope.change.imagename = file.name;
+      $scope.change.imagefile = file;
+      if ($scope.fileReaderSupported && file.type.indexOf('image') > -1) {
+        $timeout(function() {
+          var fileReader = new FileReader();
+          fileReader.readAsDataURL(file); // convert the image to data url. 
+          fileReader.onload = function(e) {
+            $timeout(function() {
+              $scope.thumbnail.dataUrl = e.target.result;
+            });
+          }
+        });
+      }
+    }
+  };
+
   $scope.saveChange = function () {
     firebase.auth().onAuthStateChanged(function (user) {
       //chage email
@@ -42,11 +71,16 @@ appControllers.controller('AccountCtrl', function ($scope, $ionicModal) {
       }
 
     });
+    //var storageRef = firebase.storage().ref().child($scope.change.userId+'/'+$scope.change.imagename);
+    storageRef.child($scope.change.userId+'/'+$scope.change.imagename).put($scope.change.imagefile).then(function(snapshot) {
+        console.log('Uploaded a blob or file!');
+    });
     firebase.database().ref('Users/' + $scope.change.userId).set({
       name: $scope.change.username,
       sex: $scope.change.usersex,
       phone: $scope.change.userphone,
-      address: $scope.change.useraddress
+      address: $scope.change.useraddress,
+      image: $scope.change.imagename
     });
     $scope.closeModal();
   };
